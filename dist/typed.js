4ec34e6fdba22f2b5f59a4d8082ae6895cacca19
Alvarado08 <jorgealvarado0238@gmail.com>
2024-02-03 16:21:16 -0600

    added typed script
    Moved Typed.js object config to separate js file in dist.

---
+++ dist/typed.js
@@ -0,0 +1,10 @@
+const titleEl = document.getElementById('title');
+let typed = new Typed('#title', {
+    stringsElement: '#typed-strings',
+    smartBackspace: true,
+    typeSpeed: 70,
+    backDelay: 700,
+    backSpeed: 100,
+    loop: true,
+    loopCount: 2,
+});
