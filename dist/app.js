b896f14b52b60271ce40f0141c98e444790e10ce
Alvarado08 <jorgealvarado0238@gmail.com>
2024-02-02 22:08:25 -0600

    UI Layout
    Started UI layout, styling and mockup.

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const searchBtn = document.querySelector('button');
+const mainContainer = document.getElementById('weather');
+const searchContainer = document.getElementById('search');
 const baseUrl = 'https://api.weatherapi.com/v1';
 const key = 'c26069194a4e4f73bac22525240501';
 function getWeatherData(city) {
@@ -31,7 +34,7 @@ function getWeatherData(city) {
         }
     });
 }
-getWeatherData("London");
+// getWeatherData("London");
 function getTemperature(data) {
     console.log(data.current.feelslike_c + ' C', data.current.feelslike_f + ' F');
 }
@@ -40,3 +43,65 @@ function getForecast(data) {
         console.log(dayF);
     });
 }
+function loadingData() {
+    mainContainer.classList.remove('grid', 'grid-cols-2', 'gap-3');
+    mainContainer.innerHTML = `
+    <div role="status" class="w-full animate-pulse">
+        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+        <span class="sr-only">Loading...</span>
+    </div>
+    `;
+}
+function showWeatherData() {
+    mainContainer.classList.remove("w-full", "flex", "justify-center");
+    mainContainer.classList.add('grid', 'grid-cols-2', 'gap-3');
+    searchContainer.classList.remove('justify-center');
+    searchContainer.classList.add('justify-end');
+    mainContainer.innerHTML = `
+        <article class="col-span-1 p-5 shadow-lg rounded backdrop-blur-lg border border-[#1F1D1B] w-full h-full text-[#1F1D1B]">
+            <h3>Today</h3>
+            <h3>London</h3>
+            <h2 class="text-4xl text-center">23°</h2>
+            <h3>Rain</h3>
+            <span>H: 24° L: 18°</span>
+        </article>
+        <div class="col-span-1 gap-3">
+            <div class="grid grid-cols-2 gap-3 w-full h-full text-[#E0E2E4]">
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    sunrise/sunset
+                </article>
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    wind
+                </article>
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    humidity
+                </article>
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    UV
+                </article>
+            </div>
+        </div>
+        <div class="col-span-2">
+            <div class="grid grid-cols-3 gap-3">
+                <article class="border border-[#1F1D1B] text-[#1F1D1B] p-5 rounded shadow-md backdrop-blur-sm w-full h-full">
+                    Mon
+                </article>
+                <article class="bg-[#1F1D1B] text-[#E0E2E4] p-5 rounded shadow-md backdrop-blur-sm w-full h-full">
+                    Tue
+                </article>
+                <article class="border border-[#1F1D1B] p-5 rounded shadow-md backdrop-blur-sm w-full h-full w-full h-full">
+                    Wed
+                </article>
+            </div>
+        </div>
+    `;
+}
+searchBtn.addEventListener('click', () => {
+    setTimeout(loadingData, 500);
+    setTimeout(showWeatherData, 2000);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-// add forecast and pending props to WeatherTemperature
-// create getForecast function
-// change getCurrent to getData
-// change getTemperature to getCurrent
+const searchBtn = document.querySelector('button') as HTMLButtonElement;
+const mainContainer = document.getElementById('weather') as HTMLDivElement;
+const searchContainer = document.getElementById('search') as HTMLDivElement;
+
 interface WeatherInfo {
     current: {
         feelslike_c: number,
@@ -55,7 +55,7 @@ async function getWeatherData(city: string): Promise<void> {
         console.log('Fetch Error:',error);
     }
 }
-getWeatherData("London");
+// getWeatherData("London");
 
 function getTemperature(data: WeatherInfo): void {
     console.log(data.current.feelslike_c + ' C', data.current.feelslike_f + ' F');
@@ -66,3 +66,68 @@ function getForecast(data: WeatherInfo): void {
         console.log(dayF);
     })
 }
+
+function loadingData(): void {
+    mainContainer.classList.remove('grid', 'grid-cols-2', 'gap-3');
+    mainContainer.innerHTML = `
+    <div role="status" class="w-full animate-pulse">
+        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+        <span class="sr-only">Loading...</span>
+    </div>
+    `;
+}
+
+function showWeatherData(): void{
+    mainContainer.classList.remove("w-full","flex","justify-center");
+    mainContainer.classList.add('grid', 'grid-cols-2', 'gap-3');
+    searchContainer.classList.remove('justify-center');
+    searchContainer.classList.add('justify-end');
+    mainContainer.innerHTML = `
+        <article class="col-span-1 p-5 shadow-lg rounded backdrop-blur-lg border border-[#1F1D1B] w-full h-full text-[#1F1D1B]">
+            <h3>Today</h3>
+            <h3>London</h3>
+            <h2 class="text-4xl text-center">23°</h2>
+            <h3>Rain</h3>
+            <span>H: 24° L: 18°</span>
+        </article>
+        <div class="col-span-1 gap-3">
+            <div class="grid grid-cols-2 gap-3 w-full h-full text-[#E0E2E4]">
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    sunrise/sunset
+                </article>
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    wind
+                </article>
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    humidity
+                </article>
+                <article class="p-5 bg-[#1F1D1B] rounded shadow-md backdrop-blur-sm">
+                    UV
+                </article>
+            </div>
+        </div>
+        <div class="col-span-2">
+            <div class="grid grid-cols-3 gap-3">
+                <article class="border border-[#1F1D1B] text-[#1F1D1B] p-5 rounded shadow-md backdrop-blur-sm w-full h-full">
+                    Mon
+                </article>
+                <article class="bg-[#1F1D1B] text-[#E0E2E4] p-5 rounded shadow-md backdrop-blur-sm w-full h-full">
+                    Tue
+                </article>
+                <article class="border border-[#1F1D1B] p-5 rounded shadow-md backdrop-blur-sm w-full h-full w-full h-full">
+                    Wed
+                </article>
+            </div>
+        </div>
+    `;
+}
+
+searchBtn.addEventListener('click', () => {
+    setTimeout(loadingData, 500);
+    setTimeout(showWeatherData, 2000);
+})
